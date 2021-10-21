5d8e9682686464dc199a48b2aa3f7c6a62846989
UWIZEYE Jean de Dieu <jeandedieuuwizeye6@gmail.com>
2021-10-21 15:34:11 +0100

    new commit

--- API/Routes/CandidatesRoute.js
+++ API/Routes/CandidatesRoute.js
@@ -3,6 +3,8 @@ bodyParser = require("body-parser");
 router = express.Router();
 
 CandidatesController = require("../controllers/CandidatesController");
+ArbitrationController = require("../controllers/ArbitrationController");
+
 
 auth = require("../authentication");
 
@@ -13,6 +15,7 @@ jsonParser = bodyParser.json();
 router.post('/register',jsonParser,CandidatesController.register)
 router.get('/all/approved',jsonParser,CandidatesController.allApproved)
 router.get('/results',jsonParser,CandidatesController.viewResults)
+router.get('/positions/',jsonParser,ArbitrationController.viewPositions)
 router.get('/position/:id',jsonParser,CandidatesController.candidatePosition)
 router.get('/all',jsonParser,auth.authenticateArbitrator,CandidatesController.all)
 router.post('/approve',jsonParser,auth.authenticateArbitrator,CandidatesController.approve)
